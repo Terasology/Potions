b0051c5124c48b005b5d8c03a0bbf4e27a1a99ab
JQChong <jingquan0302@gmail.com>
2016-12-05 15:48:32 +0800

    Add files via upload

---
+++ PotionCommonEffects.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2016 MovingBlocks
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.terasology.potions;
+
+/**
+ * This class contains a list of constants (effect IDs) used for potion-related effect type checks. Most are
+ * self-explanatory as to what they mean.
+ */
+public final class PotionCommonEffects {
+    public static final String DO_NOTHING = "NOTHING";
+
+    public static final String RESIST_PHYSICAL = "RESIST_PHYSICAL";
+	
+	public static final String INCREASE_SIZE = "INCREASE_SIZE";
+
+    public static final String HEAL = "HEAL";
+    public static final String REGEN = "REGEN";
+    public static final String TEMP_MAX_HEALTH_BOOST = "TEMP_MAX_HEALTH_BOOST";
+
+    public static final String POISON = "POISON";
+    public static final String RESIST_POISON = "RESIST_POISON";
+    public static final String CURE_POISON = "CURE_POISON";
+    public static final String CURE_ALL_AILMENTS = "CURE_ALL_AILMENTS";
+
+    public static final String WALK_SPEED = "WALK";
+    public static final String SWIM_SPEED = "SWIM";
+    public static final String JUMP_SPEED = "JUMP";
+    public static final String ITEM_USE_SPEED = "ITEM_USE_SPEED";
+    public static final String GLUE = "GLUE";
+
+    public static final String DAMAGE_REDUCE = "DAMAGE_REDUCE";
+    public static final String FEATHER_FALL = "FEATHER_FALL";
+    public static final String BRICK_FALL = "BRICK_FALL";
+    public static final String MULTI_JUMP = "MULTI_JUMP";
+
+    private PotionCommonEffects() {
+
+    }
+}
