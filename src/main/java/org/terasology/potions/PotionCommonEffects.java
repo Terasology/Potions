33199a2d24d4479edac74c4dbb86dad29be163e2
JQChong <jingquan0302@gmail.com>
2016-12-05 15:52:27 +0800

    Add files via upload

--- src/main/java/org/terasology/potions/PotionCommonEffects.java
+++ src/main/java/org/terasology/potions/PotionCommonEffects.java
@@ -23,6 +23,8 @@ public final class PotionCommonEffects {
     public static final String DO_NOTHING = "NOTHING";
 
     public static final String RESIST_PHYSICAL = "RESIST_PHYSICAL";
+	
+	public static final String INCREASE_SIZE = "INCREASE_SIZE";
 
     public static final String HEAL = "HEAL";
     public static final String REGEN = "REGEN";
