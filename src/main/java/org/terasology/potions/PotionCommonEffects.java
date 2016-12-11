aea687b4293eb94b0534256abb8a9ab10e6b5c99
digitalripperynr <coolkambojynr2@gmail.com>
2016-12-11 22:42:00 +0530

    Added BlindWalk potion.
    Added BlindWalk potion. Waiting for an API to be merged that would allow to make No_Visibility effect actually happen.

--- src/main/java/org/terasology/potions/PotionCommonEffects.java
+++ src/main/java/org/terasology/potions/PotionCommonEffects.java
@@ -44,6 +44,7 @@ public final class PotionCommonEffects {
     public static final String FEATHER_FALL = "FEATHER_FALL";
     public static final String BRICK_FALL = "BRICK_FALL";
     public static final String MULTI_JUMP = "MULTI_JUMP";
+	public static final String NO_VISIBILITY = "NO_VISIBILITY";
 
     private PotionCommonEffects() {
 
