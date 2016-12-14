8bfaada83c9270614fe0791a3c11f22696492594
digitalripperynr <coolkambojynr2@gmail.com>
2016-12-14 14:15:35 +0530

    Added BlindWalk potion

--- src/main/java/org/terasology/potions/effect/NoVisibilityEffect.java
+++ src/main/java/org/terasology/potions/effect/NoVisibilityEffect.java
@@ -15,28 +15,38 @@
  */
 package org.terasology.potions.effect;
 
-import org.terasology.entitySystem.entity.EntityManager;
 import org.terasology.entitySystem.entity.EntityRef;
-import org.terasology.entitySystem.event.ReceiveEvent;
-import org.terasology.logic.actions.*;
-import org.terasology.logic.health.*;
 import org.terasology.potions.HerbEffect;
-import org.terasology.potions.events.DrinkPotionEvent;
 
 /**
- * Once the API for Custom Actions is implemented, the code for NoVisibility Effect goes here.
- The effect will make the screen blank for the given amount of time.
+ * Copyright 2016 MovingBlocks
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Created by digitalripperynr on 14-12-2016.
  */
 public class NoVisibilityEffect implements HerbEffect {
-    /**
-     *
-     *
-     * @param instigator    The instigator who is applying this effect on the entity. It can be a herb, potion, etc.
-     * @param entity        The entity who the herb effect is being applied on.
-     * @param magnitude     The magnitude of the effect.
-     * @param duration      The duration of the effect in milliseconds.
-     
-	 Once the API for Custom Actions is implemented, the code for NoVisibility Effect goes here.
-	 */
-   
+    @Override
+    public void applyEffect(EntityRef instigator, EntityRef entity, float magnitude, long duration) {
+        /**
+         * Once the API for Custom Actions is implemented, the code for NoVisibility Effect goes here.
+         The effect will make the screen blank for the given amount of time.
+         */
+    }
+
+    @Override
+    public void applyEffect(EntityRef instigator, EntityRef entity, String id, float magnitude, long duration) {
+
+    }
+
 }
