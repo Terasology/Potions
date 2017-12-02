5ff53e1fc87d12e2c447c40c6df21423043db617
Langston Nashold <langston201@gmail.com>
2017-12-01 21:17:25 -0600

    Changed Font to NotoSans-Regular-Large

--- src/main/java/org/terasology/potions/system/PotionsCategory.java
+++ src/main/java/org/terasology/potions/system/PotionsCategory.java
@@ -91,7 +91,7 @@ public class PotionsCategory implements HelpCategory {
         TextRenderStyle titleRenderStyle = new TextRenderStyle() {
             @Override
             public Font getFont(boolean hyperlink) {
-                return Assets.getFont("engine:title").get();
+                return Assets.getFont("NotoSans-Regular-Large").get();
             }
         };
         ParagraphRenderStyle titleParagraphStyle = new ParagraphRenderStyle() {
