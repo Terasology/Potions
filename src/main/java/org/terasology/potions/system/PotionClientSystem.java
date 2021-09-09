b6e7e20dc3aa46060e590f56f7f34aa1db46ae70
darkweird <foxflameless@gmail.com>
2021-09-09 11:18:57 +0300

    feature(gestaltv7-events): replace ReceiveEvent with gestaltv7's ReceiveEvent

--- src/main/java/org/terasology/potions/system/PotionClientSystem.java
+++ src/main/java/org/terasology/potions/system/PotionClientSystem.java
@@ -4,11 +4,11 @@
 package org.terasology.potions.system;
 
 import org.terasology.engine.entitySystem.entity.EntityRef;
-import org.terasology.engine.entitySystem.event.ReceiveEvent;
 import org.terasology.engine.entitySystem.systems.BaseComponentSystem;
 import org.terasology.engine.entitySystem.systems.RegisterMode;
 import org.terasology.engine.entitySystem.systems.RegisterSystem;
 import org.terasology.engine.logic.common.DisplayNameComponent;
+import org.terasology.gestalt.entitysystem.event.ReceiveEvent;
 import org.terasology.module.inventory.ui.GetItemTooltip;
 import org.terasology.nui.widgets.TooltipLine;
 import org.terasology.potions.component.PotionComponent;
